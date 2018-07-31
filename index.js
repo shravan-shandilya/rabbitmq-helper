d92c3bb3ff0133f29994778bdf12f9b6c4c5006d
Shravan <s.shravan95@gmail.com>
2018-07-31 18:50:25 +0530

    feat: added auth while creating a producer

--- index.js
+++ index.js
@@ -1,83 +1,103 @@
-'use strict';
-const amqp = require('amqplib');
-const EventEmitter = require('events').EventEmitter;
-const util = require('util');
+"use strict";
+const amqp = require("amqplib");
+const EventEmitter = require("events").EventEmitter;
+const util = require("util");
 
 class RabbitMqProducer {
-    constructor(options){
-        if ((options == undefined) || (!options.ip) || (!options.queue)) {
-            throw new Error('incorrect options to initialize RabbitMqProducer');
-        }
-        this.queue = options.queue;
-        this.ip = options.ip;
-        amqp.connect('amqp://' + options.ip)
-        .then((conn) => {
-            return conn.createChannel();
-        })
-        .then((ch) => {
-            this.channel = ch;
-            return ch.assertQueue(options.queue, {
-                durable: true
-            });
-        })
-        .then((assertResult) => {
-            if (assertResult.queue === options.queue) {
-                return this;
-            }
-            throw new Error('requested and created queue names do not match');
-        })
-        .catch((err) => {
-            throw err;
-        })
+  constructor(options) {
+    if (
+      options == undefined ||
+      !options.ip ||
+      !options.queue ||
+      !option.user ||
+      !option.password
+    ) {
+      throw new Error("incorrect options to initialize RabbitMqProducer");
     }
-
-    send(content){
-        if (this.channel === undefined) {
-            throw new Error('rabbitmqproducer not initialized, channel doesnot exist!');
-        }
-        let channelStatus = this.channel.sendToQueue(this.queue, new Buffer(JSON.stringify(content)), {
-            persistent: true
+    this.queue = options.queue;
+    this.ip = options.ip;
+    amqp
+      .connect(
+        "amqp://" + options.user + ":" + options.password + "@" + options.ip
+      )
+      .then(conn => {
+        return conn.createChannel();
+      })
+      .then(ch => {
+        this.channel = ch;
+        return ch.assertQueue(options.queue, {
+          durable: true
         });
-        return channelStatus;
+      })
+      .then(assertResult => {
+        if (assertResult.queue === options.queue) {
+          return this;
+        }
+        throw new Error("requested and created queue names do not match");
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+
+  send(content) {
+    if (this.channel === undefined) {
+      throw new Error(
+        "rabbitmqproducer not initialized, channel doesnot exist!"
+      );
     }
+    let channelStatus = this.channel.sendToQueue(
+      this.queue,
+      new Buffer(JSON.stringify(content)),
+      {
+        persistent: true
+      }
+    );
+    return channelStatus;
+  }
 }
 
-class RabbitMqConsumer extends EventEmitter{
-    constructor(options){
-        super();
-        this.status = false;
-        amqp.connect('amqp://' + options.ip)
-        .then((conn) => {
-            return conn.createChannel();
-        })
-        .then((ch) => {
-            this.channel = ch;
-            return ch.assertQueue(options.queue, {
-                durable: true
-            });
-        })
-        .then((assertResult) => {
-            if (assertResult.queue === options.queue) {
-                this.channel.consume(this.queue,(msg) => {
-                    this.emit('data',JSON.parse(msg.content.toString()));
-                    this.channel.ack(msg);
-                },{noAck:false}); 
-                return this;
-            }
-            throw new Error('requested and created queue names do not match');
-        })
-    }
+class RabbitMqConsumer extends EventEmitter {
+  constructor(options) {
+    super();
+    this.status = false;
+    amqp
+      .connect("amqp://" + options.ip)
+      .then(conn => {
+        return conn.createChannel();
+      })
+      .then(ch => {
+        this.channel = ch;
+        return ch.assertQueue(options.queue, {
+          durable: true
+        });
+      })
+      .then(assertResult => {
+        if (assertResult.queue === options.queue) {
+          this.channel.consume(
+            this.queue,
+            msg => {
+              this.emit("data", JSON.parse(msg.content.toString()));
+              this.channel.ack(msg);
+            },
+            { noAck: false }
+          );
+          return this;
+        }
+        throw new Error("requested and created queue names do not match");
+      });
+  }
 
-    start(){
-        this.status = true;
-    }
+  start() {
+    this.status = true;
+  }
 
-    stop(){
-        this.status = false;
-    }
+  stop() {
+    this.status = false;
+  }
 }
 
 module.exports = {
-    RabbitMqConsumer: RabbitMqConsumer,
-    RabbitMqProducer: RabbitMqProducer
+  RabbitMqConsumer: RabbitMqConsumer,
+  RabbitMqProducer: RabbitMqProducer
 };
